a5a6dd8d32fed0a056606e88a69035dc8e24d84d
Het Patel <hetp111@users.noreply.github.com>
2019-01-19 16:35:19 +0530

    Create STL_HostelVisit.cpp

---
+++ codingblocks/STL_HostelVisit.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+//https://hack.codingblocks.com/contests/c/471/1629
+int main(){//;)
+    long long q,k;cin>>q>>k;
+    vector<long long> v;
+    for(long long i=0;i<q;i++){
+        long long kk,x,y;
+        cin>>kk;
+        if(kk==1){
+            cin>>x>>y;
+            v.push_back(x*x+y*y);
+        }else{
+            sort(v.begin(),v.end());
+            if(v.size()>k){
+                v.erase(v.begin()+k,v.end());
+            }
+            cout<<v[k-1]<<"\n";
+        }
+    }
+}
