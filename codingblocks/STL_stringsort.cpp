9a2a53d393b9c0afd16331d61503addf60433acf
Het Patel <hetp111@users.noreply.github.com>
2019-01-19 13:28:51 +0530

    Update STL_stringsort.cpp

--- codingblocks/STL_stringsort.cpp
+++ codingblocks/STL_stringsort.cpp
@@ -1,1 +1,33 @@
-.
+#include <bits/stdc++.h>
+using namespace std;
+//https://hack.codingblocks.com/contests/c/101/103
+//STRING SORT
+int main() {//;)
+    int n;
+    vector<string> s;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        string ss;
+        cin>>ss;
+        s.push_back(ss);
+    }
+    sort(s.begin(),s.end());
+
+    for(int i=0;i+1<n;i++){
+        int flg=0;
+        string a=s[i];
+        string b=s[i+1];
+        for(int j=0;j<min(a.size(),b.size());j++){
+            if(a[j]!=b[j]){
+                break;
+            }
+            flg=1;
+        }
+        if(flg){
+            swap(s[i],s[i+1]);
+        }
+    }
+    for(string sss:s){
+        cout<<sss<<"\n";
+    }
+}
