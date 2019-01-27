6af77c23775b80c37dbfdbc42b285bca771470b9
Het Patel <hetp111@users.noreply.github.com>
2019-01-28 01:12:16 +0530

    Create SIMPLEINPUT.cpp

---
+++ codingblocks/practice/SIMPLEINPUT.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+main()
+{
+    int sum=0;
+    while(sum>=0){
+        int n;
+        cin>>n;
+        sum+=n;
+        if(sum>=0)
+        cout<<n<<"\n";
+    }
+}
