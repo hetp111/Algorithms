fb5fc7428ddefbea82c7ac067d9ee368dae6e062
Het Patel <hetp111@users.noreply.github.com>
2019-03-29 19:24:29 +0530

    Create C-Vacation.cpp

---
+++ Dynamic_Programming/dp_atcoder/C-Vacation.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTER ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef long long ll;
+
+int main(){
+    int n;
+    int a[n+1][3];
+    int dp[n+1][3];
+    cin>>n;
+    for(int i=0;i<n;i++)for(int j=0;j<3;j++)cin>>a[i][j];
+
+    dp[0][0]=a[0][0];
+    dp[0][1]=a[0][1];
+    dp[0][2]=a[0][2];
+
+    for(int i=1;i<n;i++){
+        dp[i][0]=a[i][0]+max(dp[i-1][1],dp[i-1][2]);
+        dp[i][1]=a[i][1]+max(dp[i-1][0],dp[i-1][2]);
+        dp[i][2]=a[i][2]+max(dp[i-1][1],dp[i-1][0]);
+    }
+    cout<<max({dp[n-1][0],dp[n-1][1],dp[n-1][2]});
+}
