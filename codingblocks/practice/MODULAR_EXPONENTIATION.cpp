d95fa25af5c2deed064d5ad64bc85cbaf58f676b
Het Patel <hetp111@users.noreply.github.com>
2019-04-02 01:56:15 +0530

    Create MODULAR_EXPONENTIATION.cpp

---
+++ codingblocks/practice/MODULAR_EXPONENTIATION.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTER ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef long long ll;
+
+ll f(ll a, ll b, ll c){
+    ll rel=1;
+    while(b){
+        if(b&1)rel=(rel*a)%c;
+        a=(a*a)%c;
+        b=b>>1;
+    }
+    return rel%c;
+}
+
+int main(){
+    FASTER;
+    ll a,b,c;cin>>a>>b>>c;
+    cout<<f(a,b,c);//(a^b)%c
+}
