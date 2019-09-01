421cf4e57261e7cbeaa53b7571e25a4735bb7100
Het Patel <hetp111@users.noreply.github.com>
2019-09-01 14:13:50 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,21 +4,18 @@ using namespace std;
 #define double long double
 #define all(v) (v).begin(),(v).end()
 #define vi vector<int>
+#define vvi vector<vi>
 #define pii pair<int,int>
-#define prec(n) fixed<<setprecision(n)
 #define MOD 1000000007
 #define eps 1e-8
 #define FASTER ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-template<class A, class B> ostream& operator<<(ostream& out, const pair<A, B> &a){
-    return out<<"("<<a.first<<","<<a.second<<")";}
-template<class A> ostream& operator<<(ostream& out, const vector<A> &a){
-	out<<"";for(auto it=a.begin();it!=a.end();it++){if(it!=a.begin())out<<" ";out<<*it;}out<<"";
-	return out;}
-template<class A, class B> istream& operator>>(istream& in, pair<A,B> &a){in>>a.first>>a.second;return in;}
-template<class A> istream& operator>>(istream& in, vector<A> &a){for(A &i:a)in>>i;return in;}
+template<class A,class B>ostream&operator<<(ostream&out,const pair<A,B>&a){return out<<"("<<a.first<<","<<a.second<<")";}
+template<class A>ostream&operator<<(ostream&out,const vector<A>&a){for(const A &it:a)out<<it<<" ";return out;}
+template<class A,class B>istream&operator>>(istream&in,pair<A,B>&a){return in>>a.first>>a.second;}
+template<class A>istream&operator>>(istream&in,vector<A>&a){for(A &i:a)in>>i;return in;}
 //ifstream cinn("input.txt");ofstream coutt("output.txt");
 
 signed main(){
-    FASTER;
-
+	FASTER;
+	
 }
