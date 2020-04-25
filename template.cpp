3d64bea70f2d4a458351e8f5d88ee158f7ad435f
Het Patel <hetp111@users.noreply.github.com>
2020-04-26 02:27:39 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,10 +1,5 @@
-//This template is cursed. Don't use it!
-
 #include <bits/stdc++.h>//Carefully Crafted by hetp111
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 #define int long long
 #define double long double
 #define all(v) (v).begin(),(v).end()
@@ -12,7 +7,8 @@ using namespace __gnu_pbds;
 #define vvi vector<vi>
 #define pii pair<int,int>
 #define vii vector<pii>
-#define MOD 1000000007
+#define MOD (1000000007)
+//#define MOD (998244353)
 #define PI acos(-1)
 #define eps (1e-8)
 #define INF (1e18)
@@ -21,7 +17,6 @@ template<class A,class B>ostream&operator<<(ostream&out,const pair<A,B>&a){retur
 template<class A>ostream&operator<<(ostream&out,const vector<A>&a){for(const A &it:a)out<<it<<" ";return out;}
 template<class A,class B>istream&operator>>(istream&in,pair<A,B>&a){return in>>a.first>>a.second;}
 template<class A>istream&operator>>(istream&in,vector<A>&a){for(A &i:a)in>>i;return in;}
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 //ifstream cinn("input.txt");ofstream coutt("output.txt");
 
 signed main(){
