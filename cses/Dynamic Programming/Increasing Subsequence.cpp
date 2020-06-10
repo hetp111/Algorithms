d797ff54ae871c0eff51f35add3fa108bddbff71
Het <hetp111@users.noreply.github.com>
2020-06-10 11:29:32 +0530

    cute lis

---
+++ cses/Dynamic Programming/Increasing Subsequence.cpp
@@ -0,0 +1,46 @@
+//https://codeforces.com/blog/entry/13225
+#include <bits/stdc++.h>//Carefully Crafted by hetp111
+using namespace std;
+#define int long long
+#define double long double
+#define all(v) (v).begin(),(v).end()
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define MOD (1000000007)
+#define MOD2 (998244353)
+#define MOD3 (1000000009)
+#define PI acos(-1)
+#define eps (1e-8)
+#define INF (1e18)
+#define FASTER ios_base::sync_with_stdio(0);cin.tie(0)
+template<class A,class B>ostream&operator<<(ostream&out,const pair<A,B>&a){return out<<"("<<a.first<<","<<a.second<<")";}
+template<class A>ostream&operator<<(ostream&out,const vector<A>&a){for(const A &it:a)out<<it<<" ";return out;}
+template<class A,class B>istream&operator>>(istream&in,pair<A,B>&a){return in>>a.first>>a.second;}
+template<class A>istream&operator>>(istream&in,vector<A>&a){for(A &i:a)in>>i;return in;}
+ifstream cinn("in.txt");ofstream coutt("out.txt");
+int poww(const int &a,int b,const int &m=MOD){if(b==0)return 1;int x=poww(a,b/2,m);x=x*x%m;if(b&1)x=x*a%m;return x;}
+int ceil(const int &a,const int &b){return (a+b-1)/b;}
+////Read:
+//Use __builtin_popcountll for ll;
+//Check corner cases, n==1;
+//
+////Some function:
+//__builtin_popcountll(), 
+ 
+int n;
+multiset<int> st;
+ 
+signed main(){
+    FASTER;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        int in;cin>>in;
+        //7 3 5 3 6 2 9 8
+        auto it=st.lower_bound(in);
+        if(it!=st.end())st.erase(it);
+        st.insert(in);
+    }
+    cout<<st.size();
+}
