37da0df16e40b16bf5081507d175b4713a56d049
Het Patel <hetp111@users.noreply.github.com>
2020-08-15 01:48:23 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,3 +1,8 @@
+/*
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+*/
 #include <bits/stdc++.h>//Carefully Crafted by hetp111
 using namespace std;
 #define int long long
@@ -7,9 +12,9 @@ using namespace std;
 #define vvi vector<vi>
 #define pii pair<int,int>
 #define vii vector<pii>
-#define MOD (1000000007)
-#define MOD2 (998244353)
-#define MOD3 (1000000009)
+#define MOD 1000000007
+#define MOD2 998244353
+#define MOD3 1000000009
 #define PI acos(-1)
 #define eps (1e-8)
 #define INF (1e18)
@@ -18,17 +23,17 @@ template<class A,class B>ostream&operator<<(ostream&out,const pair<A,B>&a){retur
 template<class A>ostream&operator<<(ostream&out,const vector<A>&a){for(const A &it:a)out<<it<<" ";return out;}
 template<class A,class B>istream&operator>>(istream&in,pair<A,B>&a){return in>>a.first>>a.second;}
 template<class A>istream&operator>>(istream&in,vector<A>&a){for(A &i:a)in>>i;return in;}
-ifstream cinn("in.txt");ofstream coutt("out.txt");
+//ifstream cinn("in.in");ofstream coutt("out.out");
 int poww(const int &a,int b,const int &m=MOD){if(b==0)return 1;int x=poww(a,b/2,m);x=x*x%m;if(b&1)x=x*a%m;return x;}
 int ceil(const int &a,const int &b){return (a+b-1)/b;}
 ////Read:
-//Use __builtin_popcountll for ll;
 //Check corner cases, n==1;
 //
 ////Some function:
-//__builtin_popcountll(), 
+//__builtin_popcountll(), is_sorted(),
+
 
 signed main(){
     FASTER;
-	
+    
 }
