0a77dacc9d789f95f1396d8a92b367d0c07730c7
Het <hetp111@users.noreply.github.com>
2021-03-06 22:05:44 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -15,7 +15,7 @@ const int INF = 1e18;
 const int eps = 1e-12;
 void io(string s1 = "input.txt", string s2 = "output.txt"){freopen(s1.c_str(), "r", stdin);freopen(s2.c_str(), "w", stdout);}
 
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> new_data_set;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> pbds;
 
 signed main(){
     ios_base::sync_with_stdio(0);cin.tie(0);
