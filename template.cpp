4ec48eac1fdefc188306232f12ad556cf272a623
Het <hetp111@users.noreply.github.com>
2021-03-06 21:59:54 +0530

    removed cout<<vector

--- template.cpp
+++ template.cpp
@@ -1,37 +1,23 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimization ("unroll-loops")
-#include <bits/stdc++.h>//Carefully Crafted by hetp111
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-#define int long long
+using namespace __gnu_pbds;
+#define int int64_t
 #define double long double
-#define all(v) (v).begin(),(v).end()
+#define all(v) (v).begin(), (v).end()
 #define vi vector<int>
 #define vvi vector<vi>
-#define pii pair<int,int>
+#define pii pair<int, int>
 #define vii vector<pii>
-#define MOD 1000000007
-#define MOD2 998244353
-#define MOD3 1000000009
-#define PI acos(-1)
-#define eps (1e-8)
-#define INF (1e18)
-#define FASTER ios_base::sync_with_stdio(0);cin.tie(0)
-template<class A,class B>ostream&operator<<(ostream&out,const pair<A,B>&a){return out<<"("<<a.first<<","<<a.second<<")";}
-template<class A>ostream&operator<<(ostream&out,const vector<A>&a){for(const A &it:a)out<<it<<" ";return out;}
-template<class A,class B>istream&operator>>(istream&in,pair<A,B>&a){return in>>a.first>>a.second;}
-template<class A>istream&operator>>(istream&in,vector<A>&a){for(A &i:a)in>>i;return in;}
-//ifstream cinn("in.in");ofstream coutt("out.out");
-int poww(const int &a,int b,const int &m=MOD){if(b==0)return 1;int x=poww(a,b/2,m);x=x*x%m;if(b&1)x=x*a%m;return x;}
-int ceil(const int &a,const int &b){return (a+b-1)/b;}
-////Read:
-//Check corner cases, n==1;
-//
-////Some function:
-//__builtin_popcountll(), is_sorted(),
+const int MOD = 998244353;
+const int INF = 1e18;
+const int eps = 1e-12;
+void io(string s1 = "input.txt", string s2 = "output.txt"){freopen(s1.c_str(), "r", stdin);freopen(s2.c_str(), "w", stdout);}
 
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> new_data_set;
 
 signed main(){
-    FASTER;
+    ios_base::sync_with_stdio(0);cin.tie(0);
     
 }
