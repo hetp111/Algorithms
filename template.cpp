18ca9d75f681c2d340059e903eed4d6eb3ae7222
Het <hetp111@users.noreply.github.com>
2021-03-23 22:27:37 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
+//using namespace __gnu_pbds;
 #define int int64_t
 #define double long double
 #define all(v) (v).begin(), (v).end()
@@ -15,7 +15,7 @@ const int INF = 1e18;
 const int eps = 1e-12;
 void io(string s1 = "input.txt", string s2 = "output.txt"){freopen(s1.c_str(), "r", stdin);freopen(s2.c_str(), "w", stdout);}
 
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> pbds;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> pbds;
 
 signed main(){
     ios_base::sync_with_stdio(0);cin.tie(0);
